38a21f8c6876a520eb4cfe56a4719e97f82e7d2f
ramzarfat2006 <ramzarfat@gmail.com>
2023-12-15 13:59:07 +0700

    feat: Add declared variable

---
+++ variable/index.js
@@ -0,0 +1,24 @@
+// deklarasi Variable
+var a = 5
+var b = 10
+var c = 12
+
+let nama = "RAMZI ARFAN FATIHA"
+let umur = 17
+let tahun = 2006
+
+const luas = 10
+const diameter = 12
+const keliling = 13
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+console.log(nama)
+console.log(umur)
+console.log(tahun)
+
+console.log(luas)
+console.log(diameter)
+console.log(keliling)
