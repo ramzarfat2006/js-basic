0f71f680cfb41ae224f5ef1c60a424bb60d89dea
Ramzi Arfan Fatiha <153477198+ramzarfat2006@users.noreply.github.com>
2023-12-18 13:16:45 +0700

    Update study-case.js
    Add console.log

--- conditional/study-case.js
+++ conditional/study-case.js
@@ -3,8 +3,11 @@ let jawab;
 
 if (value > 0){
     jawab = "POSITIF"
+    console.log("BILANGAN POSITIF")
 } else if (values < 0){
     jawab = "NEGATIF"
+    console.log("BILANGAN NEGATIF")
 } else {
     jawab = "BUKAN BILANGAN"
+    console.log("BUKAN BILANGAN")
 }
