af0dfd74c3be81fd8c59fa6790f4584a2b7a690f
ramzarfat2006 <ramzarfat@gmail.com>
2023-12-18 12:30:30 +0700

    Feat: conditional in js

---
+++ conditional/study-case.js
@@ -0,0 +1,10 @@
+const value = 10
+let jawab;
+
+if (value > 0){
+    jawab = "POSITIF"
+} else if (values < 0){
+    jawab = "NEGATIF"
+} else {
+    jawab = "BUKAN BILANGAN"
+}
---
+++ conditional/switch.js
@@ -0,0 +1,10 @@
+switch ("rabu"){
+    case "senin":
+        console.log("Ini hari senin");
+        break;
+    case "selasa":
+        console.log("Ini hari selasa");
+        break;
+    default:
+        console.log("Ini bukan hari senin atau selasa");
+}
