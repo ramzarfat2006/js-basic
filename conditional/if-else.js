30018db8cb51b7bbfe89cb8d14060e19cb60e44a
Ramzi Arfan Fatiha <153477198+ramzarfat2006@users.noreply.github.com>
2023-12-18 13:14:39 +0700

    Update if-else.js
    Add console.log

--- conditional/if-else.js
+++ conditional/if-else.js
@@ -3,9 +3,12 @@ let answer
 
 if (bil == 9){
     answer = "INI BILANGAN GANJIL" 
+    console.log("INI BILANGAN GANJIL")
 } else if (bil == 4){
     answer = "INI BILANGAN GENAP"
+    console.log("INI BILANGAN GENAP")
 } else {
     answer = "BUKAN BILANGAN"
+    console.log("BUKAN BILANGAN")
 }
 
