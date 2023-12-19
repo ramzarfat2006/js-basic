d011ee0319ba076e03f058083b985eeee6597202
ramzarfat2006 <ramzarfat@gmail.com>
2023-12-19 10:37:01 +0700

    Feat:Function

---
+++ function/study-case.js
@@ -0,0 +1,24 @@
+function greetingProgram(name){
+    return "Halo Selamat Datang " + name + " di Backend Teams"
+}
+console.log(greetingProgram("Ramzi"))
+
+//buat function untuk menambah bilangan 
+function add(num1,num2){
+    return num1 + num2;
+}
+console.log(add(100,90))
+
+//buat function pengurangan,perkalian dan pembagian
+function subtraction(angka1,angka2){
+    return angka1 - angka2;
+}
+console.log(subtraction(5,10))
+function multiplication(angka1,angka2){
+    return angka1 * angka2;
+}
+console.log(multiplication(8,9))
+function division(angka1,angka2){
+    return angka1 / angka2;
+}
+console.log(division(2,100))
