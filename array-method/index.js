7ce378c29701dd6c671dc1c2724b1b22b6ff8a37
ramzarfat2006 <ramzarfat@gmail.com>
2023-12-22 14:14:37 +0700

    feat: array method

---
+++ array-method/index.js
@@ -0,0 +1,30 @@
+// function bisa disebut method
+const buah = ['orange','apple','watermelon','mango']
+
+//array.push()
+buah.push('stroberi')
+console.log(buah)
+
+//Array.toString()
+console.log(buah.toString())
+
+//latihan
+const person = ['budi','joko','wahyudi']
+
+//tambahkan 3 data dengan nama william,erik,thomas
+person.push('william','erik','thomas')
+console.log(person.toString())
+
+//array method forEach
+person.forEach(function(data){
+    console.log(data)
+})
+
+//Daily assignment / tugas harian
+
+//buat 4 array yang kemudian akan ditambahkan data dengan method push()
+//setelah data array ditambahkan, keluarkan data dan tampilkan dengan forEach()
+// 4 array dengan nama myHobbies,myFriends,myFavouriteFoods,myLanguage
+//File study-case.js
+//prefix branch: array-method
+//reviewer: RageNeko,Nurrohman
