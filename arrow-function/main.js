11d7540624d04a38dbb8f0ce09b1a7fd58f387a3
ramzarfat2006 <ramzarfat@gmail.com>
2023-12-28 10:28:27 +0700

    feat: add arrow-function study case

---
+++ arrow-function/main.js
@@ -0,0 +1,15 @@
+// deklarasi function
+function functionName(){
+    return "Halo Joko!"
+}
+// anonymous function
+const addTodo = function(){
+    return "Hello World!"
+}
+// arrow function
+const greeting = (name , age) => `Halo Namaku ${name}, Saya Berumur ${age}` 
+    "Halo Budi!"
+
+console.log(functionName())
+console.log(addTodo())
+console.log(greeting("Ramzi",18))
---
+++ arrow-function/study-case.js
@@ -0,0 +1,12 @@
+// pertambahan
+const increase = (number1,number2) => number1 + number2
+console.log(increase(5,9))
+// pengurangan
+const subtraction = (angka1,angka2) => angka1 - angka2
+console.log(subtraction(9,5))
+// pembagian
+const distribution = (nomer1,nomer2) => nomer1 / nomer2
+console.log(distribution(9,5))
+// perkalian
+const multiplication = (no1,no2) => no1 * no2
+console.log(multiplication(5,9))
