c20e5646923c50836ff45bee4482939502dcdeba
ramzarfat2006 <ramzarfat@gmail.com>
2023-12-29 16:10:20 +0700

    feat: correcting error

--- classes/study-case.js
+++ classes/study-case.js
@@ -1,7 +1,7 @@
 class CoffeShop {
     constructor(nama,menu){
         this.nama = nama
-        this.menu = [menu]
+        this.menu = (menu) ? menu : [menu]
     }
     getNama(){
         return this.nama
@@ -12,4 +12,7 @@ class CoffeShop {
 }
 const instanceCoffeShop = new CoffeShop("Gustea",["Signature Coffee","Milk Tea","Framboze Milk"])
 console.log("Nama Toko : " + instanceCoffeShop.getNama())
-console.log("Daftar Menu : " + instanceCoffeShop.getMenu())
+console.log("Daftar Menu : ")
+for (let i = 0; i <= instanceCoffeShop.getMenu().length; i++) {
+    console.log(`${i + 1}. ${instanceCoffeShop.getMenu()[i]}`);
+}
